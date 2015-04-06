3bd47977b6e6b7fee7fde512ae3eee281502eb70
HebaSakr <hebasakr_94@hotmail.com>
2015-04-07 01:34:25 +0300

    Update Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 Memory::Memory()
 {
-	currAddress = 268500992;
+	currAddress = 0;
 	memset(memory, 0, sizeof memory);
 }
 
@@ -38,18 +38,28 @@ void Memory::storeByte(unsigned int addr, char val)
 
 void Memory::storeHalf(unsigned int addr, short val)
 {
+    if (currAddress%2!=0)
+    cout<<"Error";
+    else
+    {
     addr -= 0x10010000;
     memory[addr] = val & 0xff;
     memory[addr + 1] = val >> 8;
+    }
 }
 
 void Memory::storeWord(unsigned int addr, int val)
 {
+     if (currAddress%4!=0)
+    cout<<"Error";
+    else
+    {
     addr -= 0x10010000;
     memory[addr] = val & 0xff;
     memory[addr + 1] = (val >> 8) & 0xff;
     memory[addr + 2] = (val >> 16) & 0xff;
     memory[addr + 3] = (val >> 24);
+    }
 }
 
 void Memory::storeByte(char val)
@@ -60,6 +70,7 @@ void Memory::storeByte(char val)
 
 void Memory::storeHalf(short val)
 {
+	while (currAddress%2!=0)currAddress++;
 	memory[currAddress] = val & 0xff;
 	memory[currAddress + 1] = val >> 8;
 	currAddress += 2;
@@ -67,6 +78,7 @@ void Memory::storeHalf(short val)
 
 void Memory::storeWord(int val)
 {
+	while (currAddress%4!=0)currAddress++;
 	memory[currAddress] = val & 0xff;
 	memory[currAddress + 1] = (val >> 8) & 0xff;
 	memory[currAddress + 2] = (val >> 16) & 0xff;
