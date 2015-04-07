8a22b5a099d91a3919a17f6cca49fbf73c6818d7
HebaSakr <hebasakr_94@hotmail.com>
2015-04-08 00:23:42 +0300

    Create CPU.h

---
+++ CPU.h
@@ -0,0 +1,28 @@
+
+#ifndef __Project_Assembly_1__CPU__
+#define __Project_Assembly_1__CPU__
+
+#include <iostream>
+using namespace std;
+
+class CPU
+{
+public:
+    CPU()
+    {
+        initialize();
+    }
+    int registers[32];
+    unsigned int programCounter;
+    void initialize()
+    {
+        for (int i=0; i<32; i++)
+            registers[i]=0;
+        registers[28]= 268468224;
+        registers[29]= 2147479548;
+        programCounter= 0x00400000;
+    }
+    
+};
+
+#endif /* defined(__Project_Assembly_1__CPU__) */
