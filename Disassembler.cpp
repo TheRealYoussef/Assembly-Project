dedf663beeed031c22dac6ffd1f320933a30917c
bahermursi <bahermursi@hotmail.com>
2015-04-09 00:13:33 +0300

    Update Disassembler.cpp

--- Disassembler.cpp
+++ Disassembler.cpp
@@ -1,10 +1,5 @@
 #include "Disassembler.h"
-#include <iostream>
-#include <fstream>
-#include "Instruction.h"
-#include <vector>
-#include "GLOBALS.h"
-using namespace std;
+
 disassembler::disassembler(string x){
     inFileName =x;
     getData();
@@ -39,6 +34,7 @@ void disassembler::getData(){
                 // instfile vector that stores all the binary instructions
                 instfile.push_back(instWord);
                 
+                
             }
         }
         
@@ -47,22 +43,64 @@ void disassembler::getData(){
     
 }
 void disassembler::display(){
+    labels.resize(instfile.size());
+    
+    for (int i=0; i<instfile.size();i++) {
+        address = (instfile[i]& 0x3FFFFFF) <<2;
+        offset= (address-0x00400000)/4;
+        
+        if ((instfile[i]>>26 == 2) || (instfile[i]>>26 == 3))
+            labels[offset]= "label_"+ to_string(char(offset)) +": ";
+        
+    }
     
     for (int i=0; i<instfile.size(); i++) {
+        isLui = (instfile[i]>>26 == 0x0F);
+        isOri = (instfile[i+1]>>26 == 0x0D);
+        isAddi = (instfile[i]>> 26 == 0x08);
         
-        //checking on the pseudo instructions li & la
-        isLui = (instfile[i] == 0x0F);
+        isSub = (instfile[i+1]>>26 == 0);
+        temp_rd = (instfile[i+1]>> 11) & 0x1f;
+        temp_rt = (instfile[i+1]>> 21) & 0x1f;
         
-        isOri = (instfile[i+1] == 0x0E);
+        printLi=(isLui && isOri);
         
-        isLui != isOri? printLi = false:printLi;
+        //  printLi ==1? brOffset+=2:brOffset;
         
-        inst.dissassemble(instfile[i]); // calling the disassebling process
+        printSubi=(isAddi && isSub);
         
-        cout << inst.getAssemblyInstruction() << endl;
+        // printSubi ==1? brOffset+=2:brOffset;
+        
+        
+        
+        x.dissassemble(instfile[i],temp_rd,temp_rt,printSubi,printLi,printLa); // calling the disassebling process
+        inst.push_back(x);
+        
+        
+        pc+=4;
+        
+        if (labels[i] !="" )
+            cout <<endl<<labels[i] << endl << endl;
+        
+        if (x.getAssemblyInstruction() !=""){ //j and jal
+            if (x.getOpcode() == 2 || x.getOpcode() == 3){
+                offset= ((x.getAddress()<<2)-0x00400000)/4;
+                long len1 =labels[offset].length()-1;
+                string temps1 =labels[offset];
+                cout <<endl<< x.getAssemblyInstruction() << temps1.erase(abs(len1-1)) <<endl << endl;
+            }
+            else
+                if (x.getOpcode() == 0x04 || x.getOpcode() == 0x05) {//beq and bne
+                    //   offset= (pc-0x00400000);
+                    long len=labels[x.getSignedImm()+1].length()-1;
+                    string temps = labels[x.getSignedImm()+1];
+                    cout << x.getAssemblyInstruction() <<temps.erase(abs(len-1)) <<endl;
+                }
+                else
+                    cout << x.getAssemblyInstruction() << endl;
+        }
     }
 }
-
 
 
 
