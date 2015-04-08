aa23adae003eaedb0b453a33d2adf2643f22ef90
bahermursi <bahermursi@hotmail.com>
2015-04-09 00:18:25 +0300

    Update Disassembler.h

--- Disassembler.h
+++ Disassembler.h
@@ -1,11 +1,6 @@
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include "Instruction.h"
-#include <vector>
-
 #ifndef __Mars__Disassembler__
 #define __Mars__Disassembler__
+#include "Instruction.h"
 
 class disassembler{
     
@@ -18,9 +13,25 @@ private:
     unsigned int instWord;
     ifstream inFile;
     string inFileName;
-    Instruction inst;
+    vector<Instruction> inst;
+    vector<unsigned int> instfile;
+    vector<string>labels;
+    unsigned int address;
+    int offset;
+    int brOffset=0;
+    Instruction x;
+    unsigned int pc=0x00400000;
+    int temp_rd;
+    int temp_rt;
     bool isLui;
     bool isOri;
+    bool isAddi;
+    bool isSub;
+    bool isSubu;
+    bool  printLi;
+    bool  printLa;
+    bool printSubi;
+    bool printSubiu;
     
 };
 #endif /* defined(__Mars__Disassembler__) */
