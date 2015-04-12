8eee097ee3ca0b71fad8ea6a974a9479a485d6f7
bahermursi <bahermursi@Bahers-MacBook-Pro-2.local>
2015-04-12 17:51:05 +0200

    ;

--- CPU.h
+++ CPU.h
@@ -18,7 +18,7 @@ public:
         for (int i=0; i<32; i++)
             registers[i]=0;
         registers[28]= 268468224;
-        registers[29]= 0x10010000+8192-4
+        registers[29]= 0x10010000+8192-4;
         programCounter= 0x00400000;
     }
     
